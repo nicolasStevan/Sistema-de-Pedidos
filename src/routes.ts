d2780d5a52e50b2e1980385e34abdf5834e52542
Joao <joaohenrique2@hotmail.com>
2025-04-05 23:54:41 -0300

    Captura erros requisição

--- src/routes.ts
+++ src/routes.ts
@@ -2,9 +2,9 @@ import { Router, Request, Response } from "express";
 
 const router = Router();
 
+
 router.get("/teste", (req: Request, res: Response) => {
-    res.send("Meu servidor Web Funcionando!");
-    }
-);
+   throw  new Error("Erro ao fazer essa requisição")
+    })
 
 export { router };
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,25 @@
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import { router } from './routes';
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use(router);
 
-app.listen(3333, () => {
-  console.log('Servidor Rodando na Porta 3333 !');
-}
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    return res.status(400).json({
+      message: err.message,
+    });
+  }
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+}); 
+
+app.listen(3333, () => 
+  console.log('Server is running on port 3333')
 );
