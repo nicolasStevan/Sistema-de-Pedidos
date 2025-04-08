5e4b85d30afebe452619a2f2943e26be34b09422
Joao <joaohenrique2@hotmail.com>
2025-04-08 00:34:25 -0300

    create User registration

--- src/services/user/CreateUserService.ts
+++ src/services/user/CreateUserService.ts
@@ -1,15 +1,41 @@
-
-interface UserRequest{
-    name: string;
-    email: string;
-    password: string;
+import prismaClient from "../../prisma";
+interface UserRequest {
+  name: string;
+  email: string;
+  password: string;
 }
 
-class CreateUserService{
-    async execute({name, email, password}: UserRequest){
-        console.log(name);
-        return {name: name};
+class CreateUserService {
+  async execute({ name, email, password }: UserRequest) {
+    //Verifica se o ele enviou um email
+    if (!email) {
+      throw new Error("Email incorrect");
     }
+    //Verifica se o email já está cadastrado
+    const userAlreadyExists = await prismaClient.user.findFirst({
+      where: {
+        email: email,
+      },
+    });
+    if (userAlreadyExists) {
+      throw new Error("User already exists");
+    }
+    //Cria o usuário no banco de dados
+    const user = await prismaClient.user.create({
+      data: {
+        name: name,
+        email: email,
+        password: password,
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+      },
+    });
+
+    return user;
+  }
 }
 
 export { CreateUserService };
