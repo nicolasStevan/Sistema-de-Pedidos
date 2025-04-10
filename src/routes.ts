9646b72ce69938a95517ff6735a5a1d3c5348525
Joao <joaohenrique2@hotmail.com>
2025-04-09 23:49:07 -0300

    Hashing user and  password

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import { CreateUserController } from './controllers/user/CreateUserController';
+import { AuthUserController } from './controllers/user/AuthUserController';
 
 const router = Router();
 
 // -- Rotas User --
 router.post("/users", new CreateUserController().handle);
+
+router.post("/login", new AuthUserController().handle);
 
 export { router };
