84a9905877be3f75d43ef20f11ed2c53f11d51ee
Joao <joaohenrique2@hotmail.com>
2025-05-05 22:47:09 -0300

    validation in category creation method

--- src/controllers/product/CreateProductController.ts
+++ src/controllers/product/CreateProductController.ts
@@ -1,32 +1,28 @@
-import {Request, Response } from 'express'
-import { CreateProductService } from '../../services/product/CreateProductService'
+import { Request, Response } from "express";
+import { CreateProductService } from "../../services/product/CreateProductService";
 
-
-class CreateProductController{
-  async handle(req: Request, res: Response){
-    const { name, price, description, category_id } = req.body;
+class CreateProductController {
+  async handle(req: Request, res: Response) {
+    const { name, price, description, category_id: category_id } = req.body;
 
     const createProductService = new CreateProductService();
 
-    if(!req.file){
-      throw new Error("error upload file")
-    }else{
-
+    if (!req.file) {
+      throw new Error("error upload file");
+    } else {
       const { filename: banner } = req.file;
 
       const product = await createProductService.execute({
-        
         name,
         price: Number(price),
         description,
         banner,
-        categoryId
+        category_id : category_id,
       });
-  
-      return res.json(product)
-    }
 
+      return res.json(product);
+    }
   }
 }
 
-export { CreateProductController }
+export { CreateProductController };
